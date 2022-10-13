7cce103d18731fe5d30cf89c81cea0c326581e3f
wilsb8 <wilsb8@gmail.com>
2022-10-13 04:53:22 -0700

    initial

---
+++ app/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const ejs = require('ejs');
+const morgan = require('morgan');
+const router = require("../routes/routes");
+
+// use middleware to set payload and url encoding
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true}));
+
+// use middleware for logging
+app.use(morgan('dev'));
+
+// ejs middleware
+app.set('view engine','ejs');
+app.engine('ejs', ejs.__express);
+
+// declare static file locations
+app.use(express.static('views'));
+app.use(express.static('public'));
+
+// use middleware to set our router
+app.use('/', router);
+
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const http = require("http");
 require("dotenv").config();
+const app = require('./app/app');
 
-http.createServer().listen(process.env.port, ()=> {
+http.createServer(app).listen(process.env.port, ()=> {
     console.log(`I am fully operational and all circuits are listening on ${process.env.port}`);
 });
