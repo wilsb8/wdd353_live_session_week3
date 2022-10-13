f70cb1072ee92dded30afdc67cd9f479f421093a
wilsb8 <wilsb8@gmail.com>
2022-10-13 05:02:42 -0700

    initial

--- routes/routes.js
+++ routes/routes.js
@@ -5,4 +5,21 @@ router.get("/", (req, res) => {
     res.render('home', {pagename: 'Home'});
 });
 
+router.get('/about', (req, res) => {
+    res.render('about', {
+        pagename: 'About', 
+    });
+});
+
+router.get('/contact', (req, res) => {
+    res.render('contact', {
+        pagename: 'Contact',
+    });
+});
+
+router.get('/services', (req, res) => {
+    res.render('services', {
+        pagename: 'Services',
+    });
+});
 module.exports = router;
